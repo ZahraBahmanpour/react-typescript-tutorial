0061c85b32b5902e25cc0719bfe328e102de3bb5
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-03 15:51:04 +0430

    8 - add useEffect to Store to retrieve products

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,15 @@
+import { useEffect, useState } from "react";
+import { Product } from "../model/product";
+
 export default function Store() {
+  const [products, setProducts] = useState<Product[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("http://localhost:3000/products");
+      const products = await res.json();
+      setProducts(products);
+    };
+    fetchData();
+  }, []);
   return <h1>Store</h1>;
 }

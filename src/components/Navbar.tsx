fb8e2d29edffdf4462469736a7053ab009e95c70
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-03 15:23:27 +0430

    7 - add shopping cart icon to nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,13 @@
-import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import {
+  Badge,
+  Button,
+  Container,
+  Nav,
+  Navbar as NavbarBs,
+} from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+
 export default function Navbar() {
   return (
     <NavbarBs className="bg-white shadow-sm mb-3">
@@ -15,6 +23,15 @@ export default function Navbar() {
             About
           </Nav.Link>
         </Nav>
+        <Button
+          className="rounded-circle position-relative"
+          variant="outline-primary"
+        >
+          <FaShoppingCart />
+          <Badge className="rounded-circle bg-danger position-absolute">
+            3
+          </Badge>
+        </Button>
       </Container>
     </NavbarBs>
   );

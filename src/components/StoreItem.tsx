b4ab1193c5c063b887bf6408752ac7310b376e09
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-03 16:41:19 +0430

    add buttons to storeItem

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,15 +1,40 @@
-import { Card } from "react-bootstrap";
+import { Button, Card, ToggleButton } from "react-bootstrap";
 import { Product } from "../model/product";
 
 export default function StoreItem({ id, name, price, imgUrl }: Product) {
+  const quantity = 0;
   return (
     <Card>
       <Card.Img variant="top" src={imgUrl} height="200px" />
       <Card.Body className="d-flex flex-column">
         <Card.Title className="d-flex justify-content-between align-items-baseline">
           <span className="fs-2">{name}</span>
-          <span className="ms-2 text-muted">{price}</span>
+          <span className="ms-2 text-muted">{price}$</span>
         </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100">+ Add to Cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: "0.5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: ".5rem" }}
+              >
+                <Button>-</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span> in cart
+                </div>
+                <Button>+</Button>
+              </div>
+              <Button variant="danger" size="sm">
+                Remove
+              </Button>
+            </div>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );

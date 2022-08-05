fdba19d323f7e31acfbcfeed84190ff4d5623391
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-05 08:26:31 +0430

    20 - add cutom hook

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 import ShoppingCart from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 import { Product } from "../model/product";
 
 const ShoppingCartContext = createContext({} as ShoppingCartContext);
@@ -26,7 +27,10 @@ type ShoppingCartProviderProps = {
 export type CartItem = { product: Product; quantity: number };
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+    "shopping-cart",
+    []
+  );
 
   const increaseCartQuantity = (product: Product) => {
     setCartItems((currItems) => {
